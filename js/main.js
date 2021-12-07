01b819f75326ea2ed20665c41565d6e0a95999ce
Santiagojbp <86267406+Santiagojbp@users.noreply.github.com>
2021-12-06 23:08:45 -0300

    cambios

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+let obj = [1,2]
+let val1 = obj[0]
+let val2 = obj[1]
+let [val3,val4] = obj
+console.log([obj])
+
+
+
+
+
+
+
+
 /*cargamos nuestro stock del archibo json local*/
 let stockProductos = []
 function cargandoStock() {
@@ -15,17 +28,33 @@ $.getJSON("data/stock.json", function (data) {
 /*terminamos de cargar nuestro stock*/
 function inventario(array) {
 	for(const producto of array){
-		if(producto.id <= 6)	{
+		if(producto.id <= 6){
 			$('#agregar-productos').prepend(`<div class="col-lg-2 contenedor-card" id="producto">
 												<div class="contenedorCard">
 													<div class="card img">
 													 	<img class="card__img img-card" src="${producto.imagen} " alt="Imagen de ${producto.nombre} ">
-		                                             	<div class="card__text ">
+		                                             	<div class="card__text producto-datos">
 		                                             	    <input value="${producto.id}" type="hidden">
-		                                             	    <h3 class="card__title ">${producto.nombre}</h3>
+		                                             	    <h3 class="card__title producto-nombre">${producto.nombre}</h3>
 		                                             	    <p class="precio">$${producto.precio}</p>
 		                                             	<div>
-		                                             	    <button id="boton${producto.id}" class="" style="margin-bottom:3px;margin-left:3px">agregar</button>
+		                                             	    <button id="boton${producto.id}" class="agregar-carrito" style="margin-bottom:3px;margin-left:3px">agregar<i class="bi bi-cart-plus"></i></button>
+		                                             	</div>
+													</div>
+												</div>
+											</div>
+											`)
+		}else if (producto.id > 6 & (producto.id <= 12) ) {
+			$('#agregar-productos-dos').prepend(`<div class="col-lg-2 contenedor-card" id="producto">
+												<div class="contenedorCard">
+													<div class="card img">
+													 	<img class="card__img img-card" src="${producto.imagen} " alt="Imagen de ${producto.nombre} ">
+		                                             	<div class="card__text producto-datos">
+		                                             	    <input value="${producto.id}" type="hidden">
+		                                             	    <h3 class="card__title producto-nombre">${producto.nombre}</h3>
+		                                             	    <p class="precio">$${producto.precio}</p>
+		                                             	<div>
+		                                             	    <button id="boton${producto.id}" class="agregar-carrito" style="margin-bottom:3px;margin-left:3px">agregar<i class="bi bi-cart-plus"></i></button>
 		                                             	</div>
 													</div>
 												</div>
